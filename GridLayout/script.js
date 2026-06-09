acb8c6682e71acae9e91db10be0c66c9638c49f0
Phong Su <phongdsu@gmail.com>
2026-06-09 16:49:29 -0700

    initial setup

---
+++ GridLayout/script.js
@@ -0,0 +1,26 @@
+// const wrapper = document.getElementById('scrollWrapper');
+// const content = document.getElementById('scrollContent');
+
+// Duplicate content
+// content.innerHTML += content.innerHTML;
+
+// let originalHeight;
+
+// window.addEventListener('load', () => {
+//   originalHeight = content.scrollHeight / 2;
+// });
+
+// wrapper.addEventListener('scroll', () => {
+
+//   // User reaches bottom of first set
+//   if (wrapper.scrollTop >= originalHeight) {
+//     wrapper.scrollTop -= originalHeight;
+//   }
+
+//   // User scrolls above top
+//   if (wrapper.scrollTop <= 0) {
+//     wrapper.scrollTop += originalHeight;
+
+//   }
+
+// });
